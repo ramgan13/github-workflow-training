4d2aa54376ead3710f059952112928e4a32ed8eb
ramgan13 <80527201+ramgan13@users.noreply.github.com>
2024-09-16 16:19:38 +0530

    Update update_workflow.py

--- update_workflow.py
+++ update_workflow.py
@@ -11,17 +11,21 @@ with open('services/service_names.csv', newline='') as csvfile:
         services.append(row[0])
 
 print(services)
-print(type(services))
-
-
+print("-------------------")
 
 # Load the existing workflow file
 with open('.github/workflows/choices.yaml', 'r') as file:
     workflow = yaml.safe_load(file)
 
+
+print(workflow)
+print(type(workflow))
+print("-------------------")
+
 # Ensure 'on' is treated as a string
 def preserve_strings(data):
     if isinstance(data, dict):
+        print({k: preserve_strings(v) for k, v in data.items()})
         return {k: preserve_strings(v) for k, v in data.items()}
     elif isinstance(data, list):
         return [preserve_strings(i) for i in data]
@@ -29,26 +33,30 @@ def preserve_strings(data):
         return f'"{data}"'
     return data
 
-print(workflow)
-print(type(workflow))
 
 workflow = preserve_strings(workflow)
 
 print(workflow)
 print(type(workflow))
+print("-------------------")
 
-
-#pprint(workflow)
 
 # Update the service choices
-workflow[True]['workflow_dispatch']['inputs']['service']['options'] = services
+#workflow[True]['workflow_dispatch']['inputs']['service']['options'] = services
 
 #pprint(workflow)
-print(workflow)
-print(type(workflow))
+#print(workflow)
+#print(type(workflow))
 
 # Save the updated workflow file
 with open('.github/workflows/choices.yaml', 'w') as file:
     yaml.safe_dump(workflow, file)
+    
+with open('.github/workflows/choices.yaml', 'r') as file:
+    workflow2 = yaml.safe_load(file)
+    
+print(workflow2)
+print(type(workflow2))
+
 
 print("Workflow file updated with service choices.")
