e1b966485961e248c0a6f13386b4288e6a6c24dc
ramgan13 <80527201+ramgan13@users.noreply.github.com>
2024-09-16 16:03:26 +0530

    Update update_workflow.py

--- update_workflow.py
+++ update_workflow.py
@@ -11,22 +11,24 @@ with open('services/service_names.csv', newline='') as csvfile:
         services.append(row[0])
 
 print(services)
+print(type(services))
 
 # Load the existing workflow file
 with open('.github/workflows/choices.yaml', 'r') as file:
     workflow = yaml.safe_load(file)
 
-pprint(workflow)
+#pprint(workflow)
 print(workflow)
+print(type(workflow))
 
 # Update the service choices
-workflow['on']['workflow_dispatch']['inputs']['service']['options'] = services
+#workflow['on']['workflow_dispatch']['inputs']['service']['options'] = services
 
-pprint(workflow)
-print(workflow)
+#pprint(workflow)
+#print(workflow)
 
 # Save the updated workflow file
-with open('.github/workflows/choices.yaml', 'w') as file:
-    yaml.safe_dump(workflow, file)
+#with open('.github/workflows/choices.yaml', 'w') as file:
+#    yaml.safe_dump(workflow, file)
 
-print("Workflow file updated with service choices.")
+#print("Workflow file updated with service choices.")
